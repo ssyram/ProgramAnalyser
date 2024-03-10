d0afef28007733cf1ba1c241de4afd046c5bef44
ssyram <liguany126@126.com>
2024-03-10 20:58:50 +0000

    new target files

--- paper-examples/run-tab-1.sh
+++ paper-examples/run-tab-1.sh
@@ -9,7 +9,7 @@ $program -tab:1 h-t-r-2-3-inside-score.program -concentration -m:40
 $program -tab:1 pedestrian-beta.program -direct -solver:SDP
 $program -tab:1 pedestrian-multiple-branches-v3.program -termination -degree:4
 $program -tab:1 pedestrian-multiple-branches-v4.program -termination -degree:4
-$program -tab:1 phylogenetic-model.program -no_truncate -termination -m:40 -Rp_lambda:0~2 -Rp_time:0~10
+$program -tab:1 phylogenetic-model.program -termination -m:40 -Rp_lambda:0~2 -Rp_time:0~10
 $program -tab:1 random-walk-beta-inside-scorey-v1.program -termination
 $program -tab:1 random-walk-beta-inside-scorey-v2.program -termination
 $program -tab:1 random-walk-beta-inside-scorey-v3.program -termination
