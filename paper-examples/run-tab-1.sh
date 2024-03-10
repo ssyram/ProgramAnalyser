707e30e4bf9b8c180158a0dea0c78c1132d24a17
ssyram <liguany126@126.com>
2024-03-10 21:21:34 +0000

    Update run-tab-1.sh

--- paper-examples/run-tab-1.sh
+++ paper-examples/run-tab-1.sh
@@ -2,15 +2,15 @@
 
 cd "Table 1 (fix birth)/parser-inputs1/"
 
-program="dotnet ../../../ProgramAnalyser/bin/Debug/net8.0/ProgramAnalyser.dll -O ../output"
+program="dotnet ../../../ProgramAnalyser/bin/Debug/net8.0/ProgramAnalyser.dll -O ../pt-inputs1 -tab:1"
 
-$program -tab:1 h-t-r-2-3.program -termination -m:40
-$program -tab:1 h-t-r-2-3-inside-score.program -concentration -m:40
-$program -tab:1 pedestrian-beta.program -direct -solver:SDP
-$program -tab:1 pedestrian-multiple-branches-v3.program -termination -degree:4
-$program -tab:1 pedestrian-multiple-branches-v4.program -termination -degree:4
-$program -tab:1 phylogenetic-model.program -termination -m:40 -Rp_lambda:0~2 -Rp_time:0~10
-$program -tab:1 random-walk-beta-inside-scorey-v1.program -termination
-$program -tab:1 random-walk-beta-inside-scorey-v2.program -termination
-$program -tab:1 random-walk-beta-inside-scorey-v3.program -termination
-$program -tab:1 random-walk-beta-inside-scorey-v4.program -termination
+$program race-v1.program -termination -m:40
+$program race-v2.program -concentration -m:40
+$program pd-v1.program -direct -solver:SDP
+$program pdmb-v3.program -termination -degree:4
+$program pdmb-v4.program -termination -degree:4
+$program birth.program -termination -m:40 -Rp_lambda:0~2 -Rp_time:0~10
+$program rdwalk-v1.program -termination
+$program rdwalk-v2.program -termination
+$program rdwalk-v3.program -termination
+$program rdwalk-v4.program -termination
